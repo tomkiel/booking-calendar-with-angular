c873146a3a6847dfcd1034224f0b2f29ab7729ea
tomkiel <regis@doseextra.com>
2021-08-09 03:19:10 -0300

    fix: HEROKU Server

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
-app.listen(process.env.PORT || 8080);
+
+const port = process.env.PORT || 8080;
 
 app.use(express.static(__dirname +  '/dist/reservation-calendar'));
 
 app.get('/*', (req, res) => 
   res.sendFile(__dirname + '/dist/reservation-calendar/index.html')
 );
+
+app.listen(port, function() {
+  console.log("App is running on port " + port);
+});
